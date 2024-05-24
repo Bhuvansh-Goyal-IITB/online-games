b86810bef275aac0562176d583e5724f40dd1864
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2024-05-24 13:51:39 +0530

    style (chess:ui): change from flex to grid in movelist

--- packages/ui/src/chess/MoveList.tsx
+++ packages/ui/src/chess/MoveList.tsx
@@ -13,28 +13,36 @@ function MoveList({
     <div className="flex flex-col gap-4 pb-4 mt-6 grow">
       {moveList.map(([white_move, black_move], index) => {
         return (
-          <div key={index} className="flex items-center gap-12">
+          <div key={index} className="flex items-center gap-8">
             <span>{index + 1}.</span>
-            {white_move && (
-              <Button
-                variant="link"
-                className={currentIndex - 1 == index * 2 ? "bg-secondary" : ""}
-                onClick={() => goToMove(index * 2)}
-              >
-                {white_move}
-              </Button>
-            )}
-            {black_move && (
-              <Button
-                variant="link"
-                className={
-                  currentIndex - 1 == index * 2 + 1 ? "bg-secondary" : ""
-                }
-                onClick={() => goToMove(index * 2 + 1)}
-              >
-                {black_move}
-              </Button>
-            )}
+            <div className="grid grid-cols-[5rem_5rem] lg:grid-cols-[7rem_7rem] gap-x-4 lg:gap-x-8">
+              <div className="flex">
+                {white_move && (
+                  <Button
+                    variant="link"
+                    className={
+                      currentIndex - 1 == index * 2 ? "bg-secondary" : ""
+                    }
+                    onClick={() => goToMove(index * 2)}
+                  >
+                    {white_move}
+                  </Button>
+                )}
+              </div>
+              <div className="flex">
+                {black_move && (
+                  <Button
+                    variant="link"
+                    className={
+                      currentIndex - 1 == index * 2 + 1 ? "bg-secondary" : ""
+                    }
+                    onClick={() => goToMove(index * 2 + 1)}
+                  >
+                    {black_move}
+                  </Button>
+                )}
+              </div>
+            </div>
           </div>
         );
       })}
