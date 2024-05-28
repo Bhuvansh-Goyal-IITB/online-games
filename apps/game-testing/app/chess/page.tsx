a6bcd3f41139a2d52e3811561db1e1d8a58f255b
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2024-05-28 10:32:44 +0530

    feat (game-testing): incorporate the new chess ui

--- apps/game-testing/app/chess/page.tsx
+++ apps/game-testing/app/chess/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { Chess, Move, PieceType } from "@repo/chess";
+import { Chess, Color, Move, PieceType } from "@repo/chess";
 import ChessBoard from "@repo/ui/chess/ChessBoard";
 import MoveList from "@repo/ui/chess/MoveList";
 import { PieceSet } from "@ui/chess/types";
 import { ModeToggle } from "@ui/components/mode-toggle";
 import { Button } from "@ui/components/ui/button";
-import { Card, CardContent } from "@ui/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@ui/components/ui/card";
 import {
   Dialog,
   DialogContent,
@@ -34,136 +39,81 @@ import {
 import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
-  const [chess, setChess] = useState(new Chess());
-  const [board, setBoard] = useState(chess.board);
-  const [lastMove, setLastMove] = useState<Move | null>(null);
+  const chessRef = useRef(new Chess());
 
-  const boardHistory = useRef([chess.board]);
-  const moveHistory = useRef<Move[]>([]);
-  const moveNotationList = useRef<string[][]>([]);
+  const [boardInfo, setBoardInfo] = useState(
+    chessRef.current.getBoardInfoAt(0)
+  );
 
-  const [index, setIndex] = useState(0);
-  const [flip, setFlip] = useState(false);
   const [pieceSet, setPieceSet] = useState<PieceSet>("cardinal");
 
-  const moveListDivRef = useRef<HTMLDivElement | null>(null);
+  const [lastMove, setLastMove] = useState<{
+    move: number[];
+    notation: string;
+  } | null>(null);
+  const [flip, setFlip] = useState(false);
+  const [dontAnimate, setDontAnimate] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [movesMade, setMovesMade] = useState(0);
 
-  const [animatedPiecePositions, setAnimatedPiecePositions] = useState<
-    number[] | null
-  >(null);
+  const canMakeMoves = index == movesMade && chessRef.current.outcome[0] == "";
 
-  const animationControl = (move: Move | null, isPrev: boolean) => {
-    if (move == null) {
-      setAnimatedPiecePositions(null);
-      return;
-    }
-
-    if (move.isCastling) {
-      if (move.from > move.to) {
-        isPrev
-          ? setAnimatedPiecePositions([move.from, move.to - 2])
-          : setAnimatedPiecePositions([move.to, move.to + 1]);
-      } else {
-        isPrev
-          ? setAnimatedPiecePositions([move.from, move.to + 1])
-          : setAnimatedPiecePositions([move.to, move.to - 1]);
-      }
-    } else {
-      isPrev
-        ? setAnimatedPiecePositions([move.from])
-        : setAnimatedPiecePositions([move.to]);
-    }
+  const move = (
+    from: number,
+    to: number,
+    promoteTo?: Exclude<PieceType, "k" | "p">
+  ) => {
+    if (!canMakeMoves) return;
+    chessRef.current.move(from, to, promoteTo);
+    setIndex((prev) => prev + 1);
+    setMovesMade((prev) => prev + 1);
+    setBoardInfo(chessRef.current.getBoardInfoAt(index + 1));
+    setLastMove(chessRef.current.getMoveAt(index));
+    setDontAnimate(false);
   };
 
   const handleFirst = () => {
     setIndex(0);
-    setBoard(boardHistory.current[0]!);
+    setBoardInfo(chessRef.current.getBoardInfoAt(0));
+    setDontAnimate(true);
     setLastMove(null);
-    setAnimatedPiecePositions(null);
   };
   const handlePrevious = () => {
     if (index > 0) {
       setIndex((prev) => prev - 1);
-      setBoard(boardHistory.current[index - 1]!);
-
+      setBoardInfo(chessRef.current.getBoardInfoAt(index - 1));
+      setDontAnimate(false);
       if (index == 1) {
         setLastMove(null);
       } else {
-        setLastMove(moveHistory.current[index - 2]!);
+        setLastMove(chessRef.current.getMoveAt(index - 2));
       }
     }
-    animationControl(moveHistory.current[index - 1]!, true);
   };
   const handleNext = () => {
-    if (index < boardHistory.current.length - 1) {
+    if (index < movesMade) {
       setIndex((prev) => prev + 1);
-      setBoard(boardHistory.current[index + 1]!);
-      setLastMove(moveHistory.current[index]!);
-
-      animationControl(moveHistory.current[index]!, false);
+      setBoardInfo(chessRef.current.getBoardInfoAt(index + 1));
+      setDontAnimate(false);
+      setLastMove(chessRef.current.getMoveAt(index));
     }
   };
   const handleLast = () => {
-    setIndex(boardHistory.current.length - 1);
-    setBoard(boardHistory.current.at(-1)!);
-    setLastMove(moveHistory.current.at(-1) ?? null);
-
-    setAnimatedPiecePositions(null);
-  };
-
-  //TODO: Figure out undo and animations as the piece's previous location are getting reset
-  const undo = () => {
-    if (boardHistory.current.length == 1) return;
-
-    let newIndex = boardHistory.current.length - 2;
-
-    setIndex(newIndex);
-
-    boardHistory.current.pop();
-    moveHistory.current.pop();
-
-    if (moveNotationList.current.at(-1)!.length == 1) {
-      moveNotationList.current.pop();
+    setIndex(movesMade);
+    setBoardInfo(chessRef.current.getBoardInfoAt(movesMade));
+    if (movesMade == 0) {
+      setLastMove(null);
     } else {
-      moveNotationList.current.at(-1)!.pop();
+      setLastMove(chessRef.current.getMoveAt(movesMade - 1));
     }
-
-    setChess(new Chess(boardHistory.current.at(-1)!.fen));
-    setBoard(boardHistory.current.at(-1)!);
-    setLastMove(moveHistory.current.at(-1) ?? null);
-    setAnimatedPiecePositions(null);
+    setDontAnimate(true);
   };
 
-  const goToMove = (moveIndex: number) => {
-    setIndex(moveIndex + 1);
-    setBoard(boardHistory.current.at(moveIndex + 1)!);
-    setLastMove(moveHistory.current.at(moveIndex)!);
-
-    setAnimatedPiecePositions(null);
-  };
-
-  const canMakeMoves = index == boardHistory.current.length - 1;
-
-  const movePiece = (move: Move, promoteTo?: Exclude<PieceType, "k" | "p">) => {
-    if (!canMakeMoves) return;
-    let moveNotation = chess.move(move.from, move.to, promoteTo)!;
-
-    setBoard(chess.board);
-    setIndex((prev) => prev + 1);
-    setLastMove(move);
-
-    if (moveNotationList.current.length == 0) {
-      moveNotationList.current.push([moveNotation]);
-    } else {
-      if (moveNotationList.current.at(-1)!.length == 1) {
-        moveNotationList.current.at(-1)!.push(moveNotation);
-      } else {
-        moveNotationList.current.push([moveNotation]);
-      }
-    }
-
-    boardHistory.current.push(chess.board);
-    moveHistory.current.push(move);
+  const goToMove = (index: number) => {
+    setIndex(index + 1);
+    setBoardInfo(chessRef.current.getBoardInfoAt(index + 1));
+    setLastMove(chessRef.current.getMoveAt(index));
+    setDontAnimate(true);
   };
 
   return (
@@ -172,62 +122,67 @@ export default function Page() {
         <div>
           <div className="aspect-square w-fit h-fit rounded-md overflow-hidden border-neutral-900 border-4">
             <ChessBoard
-              board={board}
-              canMakeMoves={canMakeMoves}
-              flip={flip}
-              pieceSet={pieceSet}
+              pieceList={boardInfo.pieceList}
+              enPassantPosition={
+                boardInfo.fen.split(" ")[3]! == "-"
+                  ? null
+                  : Chess.algebraic_to_position(boardInfo.fen.split(" ")[3]!)
+              }
+              currentTurn={boardInfo.fen.split(" ")[1] as Color}
+              move={move}
+              dontAnimate={dontAnimate}
               lastMove={lastMove}
-              movePiece={movePiece}
-              animatedPiecePositions={animatedPiecePositions}
-              setAnimatedPiecePositions={setAnimatedPiecePositions}
+              flip={flip}
+              validMoves={chessRef.current.validMoves}
+              canMakeMoves={canMakeMoves}
             />
           </div>
         </div>
         <div className="flex gap-2">
           <div className="grow h-full">
-            <Card className="h-full">
-              <CardContent className="h-full">
-                <div className="flex flex-col justify-between h-full">
-                  <div
-                    ref={moveListDivRef}
-                    className="flex flex-col gap-4 pb-4 mt-6 mb-4 max-h-[300px] lg:max-h-[690px] overflow-auto"
-                  >
+            <Card className="flex flex-col h-full">
+              <CardHeader>
+                <CardTitle>Move History</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col h-full grow">
+                <div className="flex flex-col gap-4 pb-4 px-3 mb-4 grow max-h-[300px] lg:max-h-[640px] overflow-auto">
+                  <div>
                     <MoveList
                       currentIndex={index}
-                      moveList={moveNotationList.current}
+                      moveList={chessRef.current.getMoveNotations()}
                       goToMove={goToMove}
                     />
                   </div>
-                  <div className="flex justify-center items-center gap-2">
-                    <Button
-                      className="p-4 py-2 lg:p-6 grow"
-                      variant="secondary"
-                      onClick={handleFirst}
-                    >
-                      <ChevronFirst className="w-6 h-6 lg:w-8 lg:h-8" />
-                    </Button>
-                    <Button
-                      className="p-4 py-2 lg:p-6 grow"
-                      variant="secondary"
-                      onClick={handlePrevious}
-                    >
-                      <ChevronLeft className="w-6 h-6 lg:w-8 lg:h-8" />
-                    </Button>
-                    <Button
-                      className="p-4 py-2 lg:p-6 grow"
-                      variant="secondary"
-                      onClick={handleNext}
-                    >
-                      <ChevronRight className="w-6 h-6 lg:w-8 lg:h-8" />
-                    </Button>
-                    <Button
-                      className="p-4 py-2 lg:p-6 grow"
-                      variant="secondary"
-                      onClick={handleLast}
-                    >
-                      <ChevronLast className="w-6 h-6 lg:w-8 lg:h-8" />
-                    </Button>
-                  </div>
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                  <Button
+                    className="p-4 py-2 lg:p-6 grow"
+                    variant="secondary"
+                    onClick={handleFirst}
+                  >
+                    <ChevronFirst className="w-6 h-6 lg:w-8 lg:h-8" />
+                  </Button>
+                  <Button
+                    className="p-4 py-2 lg:p-6 grow"
+                    variant="secondary"
+                    onClick={handlePrevious}
+                  >
+                    <ChevronLeft className="w-6 h-6 lg:w-8 lg:h-8" />
+                  </Button>
+                  <Button
+                    className="p-4 py-2 lg:p-6 grow"
+                    variant="secondary"
+                    onClick={handleNext}
+                  >
+                    <ChevronRight className="w-6 h-6 lg:w-8 lg:h-8" />
+                  </Button>
+                  <Button
+                    className="p-4 py-2 lg:p-6 grow"
+                    variant="secondary"
+                    onClick={handleLast}
+                  >
+                    <ChevronLast className="w-6 h-6 lg:w-8 lg:h-8" />
+                  </Button>
                 </div>
               </CardContent>
             </Card>
@@ -279,7 +234,7 @@ export default function Page() {
               </DialogContent>
             </Dialog>
             <div>
-              <Button variant="outline" size="icon" onClick={undo}>
+              <Button variant="outline" size="icon">
                 <Undo2 className="w-5 h-5" />
               </Button>
             </div>
