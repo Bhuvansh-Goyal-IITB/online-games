ed946439ca11a746b17b2b1a5f241eeddf39ba6e
Bhuvansh <bhuvanshgoyal2004@gmail.com>
2024-06-16 19:03:18 +0530

    build: added auth token in drizzle

--- apps/web/db/index.ts
+++ apps/web/db/index.ts
@@ -4,6 +4,7 @@ import { createClient } from "@libsql/client";
 
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 export const db = drizzle(client);
