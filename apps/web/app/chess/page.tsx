a81a03e932badea1bb5d1c8823f4a78e8c69e5b3
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-07-09 18:51:38 +0530

    fix: removed unused variable

--- apps/web/app/chess/page.tsx
+++ apps/web/app/chess/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
       </div>
       {user == undefined && <AuthPopup />}
       <div>
-        <ChessMenu id={user?.id ?? undefined} />
+        <ChessMenu />
       </div>
     </div>
   );
